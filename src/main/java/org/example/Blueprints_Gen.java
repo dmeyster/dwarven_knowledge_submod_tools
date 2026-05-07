7a01aeb3d0c4b3ff1ccc0641af8c52ba7a75af71
dmeyster <78441583+dmeyster@users.noreply.github.com>
2026-05-07 12:58:06 +0300

    added punk/blueprints generation

--- src/main/java/org/example/Blueprints_Gen.java
+++ src/main/java/org/example/Blueprints_Gen.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class Blueprints_Gen {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Dw_Kn_Modifiers_Gen modifiers_generator = new Dw_Kn_Modifiers_Gen();
         Blueprints_Gen blueprintsGen = new Blueprints_Gen();
 
-        //modifiers_generator.generateFile();
-        blueprintsGen.generateFile();
+        System.out.println("choose to generate punks/blueprints or regen modifiers files e.g. gen/regen");
+        String chosenAction = input.nextLine();
+        if (chosenAction.equals("gen")){
+            blueprintsGen.generateFile();
+        } else if (chosenAction.equals("regen")) {
+            modifiers_generator.generateFile();
+        }
     }
 }
